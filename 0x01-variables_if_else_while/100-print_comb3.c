bace1d449333935f7521210e13ff6a67b03403a1
Alisebh <korualisebh@yahoo.com>
2022-09-12 11:08:34 -0700

    corrections

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,12 +13,19 @@ int main(void)
 	e = 48;
 	while (e < 58)
 	{
-		i = 48;											while (i < 58)
-		{												if (e != i && e < i)
-			{												putchar(e);										putchar(i);										if (i == 57 && e == 56)
+		i = 48;
+		while (i < 58)
+		{
+			if (e != i && e < i)
+			{
+				putchar(e);
+				putchar(i);
+				if (i == 57 && e == 56)
 				{
 					break;
-				}											putchar(',');										putchar(' ');
+				}
+				putchar(',');
+				putchar(' ');
 			}
 			i++;
 		}
